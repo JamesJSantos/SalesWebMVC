e3bb0d0ef066785f6a89ad49ada2b2b2beecdf05
james_413 <jamesjsantos@hotmail.com>
2020-05-05 23:35:59 -0300

    Primeira model, controller e view - Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
